3541e655f362f3776f088e2c479537cfb6b02110
IRRing <majnkraftiigry7@gmail.com>
2026-05-19 23:23:19 +0300

    Bump version to 0.4.1

--- cloudtips/__init__.py
+++ cloudtips/__init__.py
@@ -6,7 +6,7 @@ from .auth import CloudTipsAuth, CloudTipsAuthError
 from .client import CloudTipsAPIError, CloudTipsClient
 from .models import AccumulationSummary, Card, Donation, PayoutFeeInfo, ReceiverProfile, TokenData
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "CloudTipsAuth",
     "CloudTipsAuthError",
